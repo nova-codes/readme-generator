ebb3861697885b46881d533c8f2a0ceca7b3a8c1
Hannah Elliott <hgelliott90@gmail.com>
2020-11-09 20:34:51 -0500

    Start the markdown generator function.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,27 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(userInput, userInfo) {
+  let defaultContents = `## Table of Contents`;
+
+  if(userInput.installation !== '') {
+    defaultContents += `* [Installation](#installation)`
+  };
+
+  if(userInput.usage !== '') {
+    defaultContents += `* [Usage](#usage)`
+  };
+
+  if(userInput.contributing !== '') {
+    defaultContents += `* [Contributing](#contributing)`
+  };
+
+  if(userInput.testing !== '') {
+    defaultContents += `* [Testing](#testing)`
+  };
+
+  return `
+  # ${userInput.title}
+
+  
 
 `;
 }
